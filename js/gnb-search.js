3ba0f1fb006700243eed445664b16c1da52011ca
Nayoung Gu <skdud000322@naver.com>
2022-10-27 16:20:01 +0900

    🚧 Implement deleting all search-history-items

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,11 +1,23 @@
 const gnbSearch = document.querySelector('.gnb-search')
 const gnbSearchInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
+const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol')
+
+const deleteAllButton = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+)
+
+console.log(gnbSearchHistoryList)
 
 function openSearchHistory() {
+  if (gnbSearchHistoryList.children.length === 0) {
+    return
+  }
+
   if (!gnbSearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeSearchHistory)
   }
+
   gnbSearchHistory.classList.add('is-active')
 }
 
@@ -16,4 +28,11 @@ function closeSearchHistory(e) {
   }
 }
 
+function deleteAllHistory() {
+  gnbSearchHistoryList.innerHTML = ''
+  gnbSearchInput.focus()
+  gnbSearchHistory.classList.remove('is-active')
+}
+
 gnbSearchInput.addEventListener('focus', openSearchHistory)
+deleteAllButton.addEventListener('click', deleteAllHistory)
